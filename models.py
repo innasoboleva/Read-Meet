cea9802c6cc85b42c2c54ba994b27ae04da70e59
Inna Soboleva <nysik62@gmail.com>
2023-08-19 12:18:18 -0700

    Created data models with SQLAlchemy, updated models.py

--- models.py
+++ models.py
@@ -1,83 +1,136 @@
-from enum import Enum
+from flask_sqlalchemy import SQLAlchemy
 
-class User:
-    """
-    Information about each unique user.
-    """
-    database =[] # temprorary holds emails
-    id_count = 0
+db = SQLAlchemy()
 
-    def __init__(self, name, email, password, zipcode, address='',age=0):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.zipcode = zipcode
-        self.address = address
-        self.age = age
-        # pre-initialization of User object properties
-        self.lists = []
-        self.host_meetings = []
-        self.guest_meetings = []
-        # getting User's id, keeping track manually
-        User.id_count += 1
-        self.id = User.id_count
 
-    @staticmethod
-    def email_exists(email):
-        if email in User.database:
-            return True
-        return False
+class User(db.Model):
+    """ A user. """
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    zipcode = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+    age = db.Column(db.Integer)
+
+    lists = db.relatioship("List", back_populates="user")
+    meetings = db.relationship("Meeting", secondary="guests", back_populates="guests")
+
+    @classmethod
+    def create(cls, email, password, name, zipcode, address=None, age=None):
+       """ Create and return a new user. """
+       return cls(email=email, password=password, name=name, zipcode=zipcode, address=address, age=age)
+
+    def __repr__(self):
+        return f'<User id={self.id} email={self.email} zipcode={self.zipcode}>'
     
-    @staticmethod
-    def add_email(email):
-        User.database.append(email)
+
+class Meeting(db.Model):
+    """ A meeting. """
+
+    __tablename__ = "meetings"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    book = db.Column(db.String, db.ForeignKey("books.ISBN"), nullable=False)
+    day = db.Column(db.DateTime, nullable=False)
+    place = db.Column(db.String)
+    address = db.Column(db.String)
+    offline = db.Column(db.Boolean, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    language = db.Column(db.String, nullable=False)
+    video_note = db.Column(db.String)
+    overview = db.Column(db.Text)
+    host_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    guests = db.relationship("User", secondary="guests", back_populates="meetings")
+
+    @classmethod
+    def create(cls, book, day, offline, host_id, video_note=None, overview=None, place=None, address=None, language="EN"):
+       """ Create and return a new meeting instance. """
+       return cls(book=book, day=day, place=place, address=address, host_id=host_id, \
+                  offline=offline, active=True, language=language, video_note=video_note, overview=overview)
+
+    def __repr__(self):
+        return f"<Meeting id={self.id} book={self.book} day={self.day} active={self.active}>"
+    
+
+class Guests(db.Model):
+    """ Middle table for users and meetings. User can attend many meetings, meetings can have many guests (users). """
+    
+    __tablename__ = "guests"
+
+    meeting_id = db.Column(db.Integer, db.ForeignKey("meetings.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    
+
+class Book(db.Model):
+    """ A book. """
+
+    __tablename__ = "books"
+
+    ISBN = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    authors = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text)
+    book_url = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+
+    lists = db.relationship("List", secondary="books_lists", back_populates="books")
+
+    @classmethod
+    def create(cls, isbn, title, authors, book_url, image_url=None, description=None):
+       """ Create and return a new book. """
+       return cls(ISBN=isbn, title=title, authors=authors, book_url=book_url, image_url=image_url, description=description)
+
+    def __repr__(self):
+        return f"<Book ISBN={self.ISBN} title={self.title} authors={self.authors}>"
+    
+
+class List(db.Model):
+    """ A list of books. """
+
+    __tablename__ = "lists"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    user = db.relationship("User", back_populates="lists")
+    books = db.relationship("Book", secondary="books_lists", back_populates="lists")
+
+    @classmethod
+    def create(cls, name, user):
+       """ Create and return a new list. """
+
+       return cls(name=name, user=user)
+
+    def __repr__(self):
+        return f"<List name={self.name} user={self.user_id}>"
+    
+
+class BookList(db.Model):
+    """ Associate table for connecting lists and books, many-to-many relationship. """
+
+    __tablename__ = "books_lists"
+
+    book_id = db.Column(db.String, db.ForeignKey("books.ISBN"), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey("lists.id"), nullable=False)
 
 
-class Meeting:
-    """
-    Information about each meeting.
-    """
-    def __init__(self, host, book, date, time, place, language, offline=False):
-        self.host = host
-        self.book = book
-        self.date = date
-        self.time = time
-        self.place = place
-        self.language = language
-        self.offline = offline
-        # pre-initialization of Meeting object properties
-        self.guests = []
-        self.active = True
+def connect_to_db(flask_app, db_uri="postgresql:///meetbookclub", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+    print("Connected to the db!")
 
 
-class List:
-    """
-    User's list to keep track of books.
-
-    Consist of a single array of Books.
-    """
-    def __init___(self, name):
-        self.name = name
-        # pre-initialization of List object properties
-        self.books = []
-
-
-class Book:
-    """
-    Basic information about the book.
-    """
-
-    def __init__(self, title, author, description, avg_rating=0):
-       self.author = author
-       self.title = title
-       self.description = description
-       self.avg_rating = avg_rating
-
-
-class Rating(Enum):
-    ZERO_ = 0
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
+if __name__ == "__main__":
+    from server import app
+    # echo=False: not to print out every query it executes.
+    connect_to_db(app)
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+import os
 from apis import books_api, yelp_api, goodreads_api
 from flask import Flask, render_template
+from models import connect_to_db, db, User
 
 app = Flask(__name__)
-app.secret_key = "SECRETSECRETSECRET"
+app.secret_key = os.environ.get('FLASK_KEY')
 
 
 @app.route("/")
@@ -13,4 +15,6 @@ def homepage():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost')
+    connect_to_db(app)
+    app.run(debug=True, host='localhost') # localhost preferable for video api
+    
