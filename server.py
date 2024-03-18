9623de253c86282f6fc9af6f73df2be7807f35be
Inna Soboleva <nysik62@gmail.com>
2024-03-18 15:20:36 -0700

    Setting prod

--- server.py
+++ server.py
@@ -8,9 +8,7 @@ import pytz
 
 
 app = Flask(__name__)
-with app.app_context():
-    connect_to_db(app)
-    db.create_all()
+
 
 # app.app_context().push()
 app.secret_key = os.environ.get('FLASK_KEY')
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from server import app
+from models import connect_to_db, db
 
 if __name__ == '__main__':
     app.run(debug=False)
+
+    with app.app_context():
+        connect_to_db(app)
+        db.create_all()
