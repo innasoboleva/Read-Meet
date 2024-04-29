8ca16e25b8c49dae1dae72f7c4ce02cadf03d55a
Inna Soboleva <nysik62@gmail.com>
2024-04-29 12:08:06 -0700

    Added file for updating books monthly

--- popular_books_seed.py
+++ popular_books_seed.py
@@ -4,12 +4,11 @@ from server import app
 
 with app.app_context():
     connect_to_db(app)
-    db.create_all()
     # every month in prod
     crud.add_new_popular_books_to_db()
     # updating old meetings to inactive
     crud.update_past_meetings()
-    # save changes
-    db.session.commit()
     # every month books needs to be uploaded
     crud.delete_old_unused_books_from_db()
+    # save changes
+    db.session.commit()
